c2b4b954afef77b3c80b81e4a7a7d0497bd01412
cyr <joakim.nilsson@gmail.com>
2014-06-07 11:28:55 +0200

    Another test added for ArrayReader.

--- SimpleDAL.MSSQLRepository.Tests/Helpers/ArrayReaderTests.cs
+++ SimpleDAL.MSSQLRepository.Tests/Helpers/ArrayReaderTests.cs
@@ -12,15 +12,7 @@ namespace SimpleDAL.Helpers
     {
         private const long LARGE_BUF_SIZE = 2048;
         private const long SMALL_BUF_SIZE = 2;
-
-        private long TestReadFuncSmall<T>(int ordinal, long fieldOffset, T[] buf, int bufOffset, int length)
-        {
-            T[] testBuffer = new T[SMALL_BUF_SIZE];
-
-            testBuffer.CopyTo(buf, bufOffset);
-
-            return testBuffer.Length;
-        }
+        private const long DEFAULT_BUF_SIZE = 1024;
 
         [TestMethod]
         public void CheckArraySizeCorrectUnderDefaultSize()
@@ -30,11 +22,44 @@ namespace SimpleDAL.Helpers
             Assert.AreEqual(SMALL_BUF_SIZE, resultBuf.Length);
         }
 
+
+        [TestMethod]
+        public void CheckArraySizeCorrectOverDefaultSize()
+        {
+            var resultBuf = ArrayReader.GetArray<byte>(TestReadFuncLarge, 0);
+
+            Assert.AreEqual(LARGE_BUF_SIZE, resultBuf.Length);
+        }
+
+        [TestMethod]
+        public void CheckArraySizeCorrectExactlyDefaultSize()
+        {
+            var resultBuf = ArrayReader.GetArray<byte>(TestReadFuncDefault, 0);
+
+            Assert.AreEqual(DEFAULT_BUF_SIZE, resultBuf.Length);
+        }
+
+        private long TestReadFuncDefault<T>(int ordinal, long fieldOffset, T[] buf, int bufOffset, int length)
+        {
+            T[] testBuffer = new T[DEFAULT_BUF_SIZE];
+
+            return CopyToArray(fieldOffset, buf, testBuffer);
+        }
+
         private long TestReadFuncLarge<T>(int ordinal, long fieldOffset, T[] buf, int bufOffset, int length)
         {
             T[] testBuffer = new T[LARGE_BUF_SIZE];
 
             return CopyToArray(fieldOffset, buf, testBuffer);
+        }
+
+        private long TestReadFuncSmall<T>(int ordinal, long fieldOffset, T[] buf, int bufOffset, int length)
+        {
+            T[] testBuffer = new T[SMALL_BUF_SIZE];
+
+            testBuffer.CopyTo(buf, bufOffset);
+
+            return testBuffer.Length;
         }
 
         private static long CopyToArray<T>(long offset, T[] buf, T[] testBuffer)
@@ -50,14 +75,6 @@ namespace SimpleDAL.Helpers
                 ++copiedElements;
             }
             return copiedElements;
-        }
-
-        [TestMethod]
-        public void CheckArraySizeCorrectOverDefaultSize()
-        {
-            var resultBuf = ArrayReader.GetArray<byte>(TestReadFuncLarge, 0);
-
-            Assert.AreEqual(LARGE_BUF_SIZE, resultBuf.Length);
         }
     }
 }
